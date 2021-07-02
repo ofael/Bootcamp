0f58381e75da64bac8ae831f8e2aa404ee479c0a
ofael <fael_assis@hotmail.com>
2021-07-02 14:17:14 -0300

    created test groupBy

--- api-collections/src/main/kotlin/collections/TesteList.kt
+++ api-collections/src/main/kotlin/collections/TesteList.kt
@@ -1,26 +1,37 @@
 package collections
 
 fun main(){
-    val rafael = Funcionarios(nome = "Rafael", salario = 3500.0)
-    val maria = Funcionarios(nome = "Maria", salario = 1500.0)
-    val ghessica = Funcionarios(nome = "Ghessica", salario = 4500.0)
+    //criando objetos
+    val rafael = Funcionarios(nome = "Rafael", salario = 3500.0, "CLT")
+    val maria = Funcionarios(nome = "Maria", salario = 1500.0,"PJ")
+    val ghessica = Funcionarios(nome = "Ghessica", salario = 4500.0,"CLT")
 
+    //criando lista de funcionarios
     val funcionarios = listOf(rafael,maria,ghessica)
-
+    //percorrendo a lista de funcionarios e imprimindo valor
     funcionarios.forEach {
         println(it)
     }
     println("~~~~~~~~~~")
+    //procurando funcionario com nome especifico
     println(funcionarios.find { it.nome == "Maria" })
     println("~~~~~~~~~~")
+    //ordenando funcionarios pelo salario
          funcionarios
         .sortedBy { it.salario }
         .forEach{println(it)}
+    println("~~~~~~~~~~")
+    //ordenando funcionarios pelo tipo de contratação
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach {  println(it) }
+    
 }
 
 data class Funcionarios(
   val nome:String,
-  val salario:Double
+  val salario:Double,
+  val tipoContratacao:String
 ){
     override fun toString():String = """
         Nome: $nome
