9356869f740d6af47066953e71f8ad8972d1d2eb
ofael <fael_assis@hotmail.com>
2021-07-02 11:55:26 -0300

    creating new test DoubleArray and doubleArrayOf

---
+++ api-collections/src/main/kotlin/collections/TesteDoubleArray.kt
@@ -0,0 +1,29 @@
+package collections
+
+fun main(){
+    val salarios = DoubleArray(3)
+    salarios[0] = 1000.0
+    salarios[1] = 3000.0
+    salarios[2] = 500.0
+
+    for(salario in salarios){
+        println (salario)
+    }
+    println("~~~~~~~~~~")
+    salarios.sort()
+    salarios.forEach {
+        println(it)
+    }
+    println("~~~~~~~~~~")
+    salarios.forEachIndexed{index, salario -> salarios[index] += salario * 0.1 }
+
+    salarios.forEach { println(it) }
+
+    println("~~~~~~~~~~")
+
+    val salarios2 = doubleArrayOf(1000.0,5500.0,3000.0)
+    salarios2.sort()
+    salarios2.forEach {
+        println(it)
+    }
+}
