029b76fd9d4af67568553bf7a060c941f881e4eb
ofael <fael_assis@hotmail.com>
2021-07-02 12:08:29 -0300

    creating new test, testing operation with max,min,average and filter

---
+++ api-collections/src/main/kotlin/collections/TesteOperacoes.kt
@@ -0,0 +1,24 @@
+package collections
+
+fun main(){
+    val salarios = doubleArrayOf(1500.0,2000.0,3780.0)
+
+    for ( salario in salarios){
+        println(salario)
+    }
+
+    println("~~~~~~~~~~")
+    //maior salario
+    println("Mario salario: ${salarios.max()}")
+    //menor salario
+    println("Menor salario: ${salarios.min()}")
+    //media dos salarios
+    println("Media dos salarios: ${salarios.average()}")
+
+    //filtrando valores
+    val salarioMaiorQue2500 = salarios.filter { it > 2500 }
+    println("~~~~~~~~~~")
+    salarioMaiorQue2500.forEach {
+        println("Maior salario: $it")
+    }
+}
